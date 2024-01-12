7c93fd8a4336d24bc77e7b0c5da30ebabf94334c
Vineet Singh <vineet.singh14204@gmail.com>
2024-01-12 09:47:18 -0600

    Update text to speach.py

--- text to speach.py
+++ text to speach.py
@@ -1,6 +1,6 @@
-from gtts import gTTS 
+from gtts import gTTS   #importing GTTS module
 import os
-f=open(r"talking to the moon.txt","r")
+f=open(r"talking to the moon.txt","r") # opening text file in read mode
 a=f.read()
 print(a)
 language = 'en'
